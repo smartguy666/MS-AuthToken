419bdd5cf1486d9359de38f6aa776af0a44eb1b7
Taimoor Alam <talam@xavor.com>
2020-03-04 12:47:32 +0500

    update: MS graph and SP separate functions added

--- MS-AuthToken/GenerateAuthToken.py
+++ MS-AuthToken/GenerateAuthToken.py
@@ -5,7 +5,7 @@ class AuthTokenGenerator():
     def GenerateAuthTokenCustomConfig(self, config):
         """
         Generate Microsoft authentication token for graph api and SharePoint rest api.
-        
+
         Sample config to pass:
         
         config = {
@@ -20,25 +20,73 @@ class AuthTokenGenerator():
 
         """
 
-        # username = request.data.get('username')
-        # password = request.data.get('password')
-
-        ## Sample config to send
-        # config = {
-        #     'grant_type': 'password',
-        #     'client_id': settings.AZURE_CLIENT_ID,
-        #     'client_secret': settings.AZURE_APP_SECRET,
-        #     'userName': username,
-        #     'password': password,
-        #     'scope': settings.AZURE_AUTH_SCOPES,
-        #     'TenantID': settings.AZURE_TENANT
-        # }
-
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded',
             'SdkVersion': 'postman-graph/v1.0'
         }
         _url = "https://login.microsoftonline.com/{0}/oauth2/v2.0/token".format(config.TenantID)
+        _body = config
+        _response = requests.post(_url, headers=headers, data=_body)
+        if 'access_token' in _response.json():
+            access_token = _response.json()['access_token']
+            return access_token
+        else:
+            return _response.json()
+
+
+
+    def GenerateAuthTokenSharepoint(self, client_id, client_secret, userName, password, TenantID, tenantName):
+        """
+        Generate authentication token for SharePoint rest api.
+        """
+
+        config = {
+            'grant_type': 'password',
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'userName': userName,
+            'password': password,
+            'scope': 'openid https://{0}.sharepoint.com/.default'.format(tenantName), 
+            'TenantID': TenantID
+        }
+
+
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'SdkVersion': 'postman-graph/v1.0'
+        }
+        _url = "https://login.microsoftonline.com/{0}/oauth2/v2.0/token".format(TenantID)
+        _body = config
+        _response = requests.post(_url, headers=headers, data=_body)
+        if 'access_token' in _response.json():
+            access_token = _response.json()['access_token']
+            return access_token
+        else:
+            return _response.json()
+
+
+
+    def GenerateAuthTokenGraphApi(self, client_id, client_secret, userName, password, TenantID, tenantName):
+        """
+        Generate authentication token for MS Graph api.
+        """
+
+        config = {
+            'grant_type': 'password',
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'userName': userName,
+            'password': password,
+            'scope': 'openid https://graph.microsoft.com/.default',
+            'TenantID': TenantID
+        }
+
+
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'SdkVersion': 'postman-graph/v1.0'
+        }
+        _url = "https://login.microsoftonline.com/{0}/oauth2/v2.0/token".format(TenantID)
         _body = config
         _response = requests.post(_url, headers=headers, data=_body)
         if 'access_token' in _response.json():
